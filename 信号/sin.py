9ddae9a954b93d45a96cdd17701d2bedec9c5559
Soar-cxx <864954796@qq.com>
2020-10-16 13:01:28 +0800

    master

---
+++ 信号/sin.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+#coding=utf-8
+
+import matplotlib.pyplot as plt
+import numpy as np 
+# linspace 第一个参数序列起始值, 第二个参数序列结束值,第三个参数为样本数默认50
+x = np.linspace(0, 3 * np.pi, 100)
+y = np.sin(x)
+
+plt.rcParams['font.sans-serif']=['SimHei'] #加上这一句就能在图表中显示中文
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+plt.subplot(1,2,1)
+plt.title(r'$f(x)=sin(x)$') 
+plt.plot(x, y)
+plt.show()
